5ebba711d73ddc111f49dafc0850390915c035b8
hovirix <contact@hovirix.dev>
2026-09-09 19:21:44 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "OSSend development shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          biome
+          nodejs_24
+          pnpm
+        ];
+      };
+    };
+}
